e404f3d93e67c69fdf51b7b974e7806d41a661be
Zayn Baig <zayn.baig@gmail.com>
2023-08-02 08:31:53 -0400

    Ready. Set. Go!

---
+++ src/hello-world-in-ts.class.ts
@@ -0,0 +1,7 @@
+export class HelloWorldInTs {
+  static HelloWorld() {
+    let h1 = document.createElement("h1");
+    h1.innerText = "Hello World in ts";
+    document.body.appendChild(h1);
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,4 @@
+import { HelloWorldInTs }
+  from './hello-world-in-ts.class';
+
+HelloWorldInTs.HelloWorld();
